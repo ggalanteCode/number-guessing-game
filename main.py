e0ef64db8a87dba860053e485d42700f3a2e02df
ggalanteCode <ggalante3@gmail.com>
2025-04-03 18:30:27 +0200

    the upper bound must be considered as number to be guessed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print("*** WELCOME TO THE NUMBER GUESSING GAME! ***")
 lower_bound = int(input("enter the lower bound: "))
 upper_bound = int(input("enter the upper bound: "))
 
-number_to_be_guessed = randrange(lower_bound, upper_bound)
+number_to_be_guessed = randrange(lower_bound, upper_bound + 1)
 
 chances = ceil(log2(upper_bound - lower_bound + 1))
 
