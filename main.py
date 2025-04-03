76d9ca7f75fec77d6ee9dbc07843cc59de84c9da
ggalanteCode <ggalante3@gmail.com>
2025-04-03 17:08:58 +0200

    lower and upper bound are integers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("*** WELCOME TO THE NUMBER GUESSING GAME! ***")
 
-lower_bound = input("enter the lower bound: ")
-upper_bound = input("enter the upper bound: ")
+lower_bound = int(input("enter the lower bound: "))
+upper_bound = int(input("enter the upper bound: "))
