65047dc6ffa9b33cbe8096e2ba692e4bd5a23d9d
ggalanteCode <ggalante3@gmail.com>
2025-04-03 17:12:40 +0200

    generate random number in the selected range of values

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from random import randrange
+
 print("*** WELCOME TO THE NUMBER GUESSING GAME! ***")
 
 lower_bound = int(input("enter the lower bound: "))
 upper_bound = int(input("enter the upper bound: "))
+
+number_to_be_guessed = randrange(lower_bound, upper_bound)
