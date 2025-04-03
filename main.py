bcbc9b87901f5e8a77f08602a3c9104246ec8748
ggalanteCode <ggalante3@gmail.com>
2025-04-03 18:15:24 +0200

    using f-strings for print and input

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ number_to_be_guessed = randrange(lower_bound, upper_bound)
 
 chances = ceil(log2(upper_bound - lower_bound + 1))
 
-print("You have", chances, "chances to guess the number!")
+print(f"You have {chances} chances to guess the number!")
 
 guess_counter = 0
 
@@ -18,7 +18,7 @@ while guess_counter < chances:
 
     guess_counter += 1
 
-    user_guess = int(input("try to guess the number: "))
+    user_guess = int(input(f"attempt n. {guess_counter}: try to guess the number: "))
 
     if user_guess == number_to_be_guessed:
         print("congratulations! you guessed the number! YOU WIN!!!")
