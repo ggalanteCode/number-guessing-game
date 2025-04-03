93055c30da802d17fc97476772fe13a8e91e8178
ggalanteCode <ggalante3@gmail.com>
2025-04-03 17:31:43 +0200

    print the number of chances

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ upper_bound = int(input("enter the upper bound: "))
 number_to_be_guessed = randrange(lower_bound, upper_bound)
 
 chances = ceil(log2(upper_bound - lower_bound + 1))
+
+print("You have", chances, "chances to guess the number!")
