9f510ecac9b4216e94a07338bdd82188ffc26bff
ggalanteCode <ggalante3@gmail.com>
2025-04-03 17:56:23 +0200

    let the user try to guess the number using the number of chances

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ number_to_be_guessed = randrange(lower_bound, upper_bound)
 chances = ceil(log2(upper_bound - lower_bound + 1))
 
 print("You have", chances, "chances to guess the number!")
+
+while chances > 0:
+
+    user_guess = int(input("try to guess the number: "))
+
+    if user_guess == number_to_be_guessed:
+        print("congratulations! you guessed the number! YOU WIN!!!")
+        break
+
+    if user_guess > number_to_be_guessed:
+        print("sorry! you guessed too high...")
+    elif user_guess < number_to_be_guessed:
+        print("sorry! you guessed too small...")
+
+    chances -= 1
+
+else:
+    print("sorry! YOU LOSE...")
