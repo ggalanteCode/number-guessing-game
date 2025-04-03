f0bb97ccb7786fae76959266b61229f1886426d2
ggalanteCode <ggalante3@gmail.com>
2025-04-03 17:30:11 +0200

    calculate the number of chances the user has to guess the number

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randrange
+from math import ceil, log2
 
 print("*** WELCOME TO THE NUMBER GUESSING GAME! ***")
 
@@ -6,3 +7,5 @@ lower_bound = int(input("enter the lower bound: "))
 upper_bound = int(input("enter the upper bound: "))
 
 number_to_be_guessed = randrange(lower_bound, upper_bound)
+
+chances = ceil(log2(upper_bound - lower_bound + 1))
